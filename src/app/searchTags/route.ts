c55dfb0f3ceeebb4e17395c2be2911c4a8234a36
kgpksh <tkdgns543@gmail.com>
2024-09-26 06:52:04 +0900

    feat: 임시 태그 검색 api 구현

---
+++ src/app/searchTags/route.ts
@@ -0,0 +1,10 @@
+import { NextRequest, NextResponse } from "next/server";
+const MAX_RESULT_NUM = 10
+const DEV_TAGS: string[] = ['SaaS', 'Mobile', 'AI', 'asdf', 'sss', 'sadfs', 'ASDHFKALS','adsfkh', 'dsahfksadf', 'sadhfas', 'sadfh;',' dsahlkf']
+
+export async function GET(req: NextRequest) {
+    const word = req.nextUrl.searchParams.get('word')
+    const searchingWord = word ? word : ""
+    const response: string[] = DEV_TAGS.filter((tag: string) => tag.includes(searchingWord))
+    return NextResponse.json(response.slice(0, MAX_RESULT_NUM))
+}
