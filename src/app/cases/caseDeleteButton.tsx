b30442734bad29f922de3207715c10fd95a4f57b
kgpksh <tkdgns543@gmail.com>
2024-10-16 13:59:27 +0900

    feat: 게시글 삭제버튼 구현

---
+++ src/app/cases/caseDeleteButton.tsx
@@ -0,0 +1,37 @@
+"use client"
+
+import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "@/components/ui/alert-dialog";
+import { Button } from "@/components/ui/button";
+import { createClient } from "@/utils/supabase/client";
+import { useRouter } from "next/navigation";
+
+export default async function CaseDeleteButton({ id }: { id: number }) {
+    const {push, refresh} = useRouter()
+    const onSubmit = async () => {
+        const supabase = createClient()
+        const {data, error} = await supabase
+                                    .from('fail_cases')
+                                    .delete()
+                                    .eq('id', id)
+        
+        push('/')
+        refresh()
+    }
+    return (
+        <AlertDialog>
+            <AlertDialogTrigger asChild className="ml-2">
+                <Button variant="outline">Delete</Button>
+            </AlertDialogTrigger>
+            
+            <AlertDialogContent>
+                <AlertDialogHeader>
+                    <AlertDialogTitle>Are you sure to delete this case?</AlertDialogTitle>
+                </AlertDialogHeader>
+                <AlertDialogCancel>Cancel</AlertDialogCancel>
+                <AlertDialogAction onClick={onSubmit}>
+                    Delete
+                </AlertDialogAction>
+            </AlertDialogContent>
+        </AlertDialog>
+    )
+}
